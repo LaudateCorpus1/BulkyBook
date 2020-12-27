72478798a086f39d91c0fd751fe82bfe10a69b10
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-12-27 14:18:58 +0000

    Implemented Facebook Login.

--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -45,6 +45,11 @@ namespace BulkyBook
                 options.LogoutPath = $"/Identity/Account/Logout";
                 options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
             });
+            services.AddAuthentication().AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
