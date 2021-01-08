d10226355a34c143a2c86d037eb34ba5da641b89
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2021-01-08 18:58:39 +0000

    Oder controller, Index and js file created.

---
+++ BulkyBook/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _uow;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _uow = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetOrderList(string status)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            IEnumerable<OrderHeader> orderHeadersList;
+
+            if (User.IsInRole(SD.Roles.Admin) || User.IsInRole(SD.Roles.Employee))
+            {
+                orderHeadersList = _uow.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                orderHeadersList = _uow.OrderHeader.GetAll(a => a.ApplicationUserId == claim.Value, includeProperties: "ApplicationUser");
+            }
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeadersList = orderHeadersList.Where(a => a.PaymentStatus == SD.PaymentStatus.Pending);
+                    break;
+                case "completed":
+                    orderHeadersList = orderHeadersList.Where(a => a.OrderStatus == SD.OrderStatus.Approved || a.OrderStatus == SD.OrderStatus.Processing || a.OrderStatus == SD.OrderStatus.Pending);
+                    break;
+                case "inprocess":
+                    orderHeadersList = orderHeadersList.Where(a => a.OrderStatus == SD.OrderStatus.Shipped);
+                    break;
+                case "rejected":
+                    orderHeadersList = orderHeadersList.Where(a => a.OrderStatus == SD.OrderStatus.Cancelled || a.OrderStatus == SD.OrderStatus.Refunded || a.PaymentStatus == SD.PaymentStatus.Rejected);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeadersList });
+        }
+    }
+}
---
+++ BulkyBook/wwwroot/js/order.js
@@ -0,0 +1,72 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("inprocess"))
+        loadDataTable("GetOrderList?status=inprocess");
+    else if (url.includes("pending"))
+        loadDataTable("GetOrderList?status=pending");
+    else if (url.includes("completed"))
+        loadDataTable("GetOrderList?status=completed");
+    else if (url.includes("rejected"))
+        loadDataTable("GetOrderList?status=rejected");
+    else if (url.includes("all"))
+        loadDataTable("GetOrderList?status=all");
+})
+
+function loadDataTable(url) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/order/" + url,
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "id", "width": "15%" },
+            { "data": "name", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/Admin/Order/Details/${data}" class="btn-success" style="cursor:pointer">    <i class="fas fa-user-edit"></i> &nbsp;</a>
+                             </div>`
+                },
+                "width": "5%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No data found"
+        },
+        "width": "100%"
+    })
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover.",
+        icon: "warning",
+        dangerMode: true,
+        buttons: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
+}
