a2c519f41ba89c49c17a52337ae234a10b4958c3
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2021-01-14 11:27:22 +0000

    Implemented View Component to display user name and the component will take a model. If you see the layout page there is no model currently. You can get the model using View component.

---
+++ BulkyBook/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _uow;
+
+        public UserNameViewComponent(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var userFromDB = _uow.ApplicationUser.GetFirstOrDefault(a => a.Id == claims.Value);
+
+            return View(userFromDB);
+        }
+    }
+}
